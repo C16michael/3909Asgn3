c9d40fa0f4b1a8484d6fa888e2a0a20af5fb2ef0
Cristiano Michael <c16michael@gmail.com>
2023-11-18 23:58:59 -0600

    Works Fully

--- reactive_example.js
+++ reactive_example.js
@@ -1,60 +1,76 @@
+// Cristiano Michael 3147571
 const express = require("express");
 const mongodb = require("mongodb");
 const expressWs = require("express-ws");
 const { MONGODB } = require("./credentials");
 
+// MongoDB connection URI
 const uri = `mongodb+srv://${MONGODB.user}:${MONGODB.login}@${MONGODB.cluster}/?retryWrites=true&w=majority`;
 const client = new mongodb.MongoClient(uri);
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'pug');
-app.set('views', './views');
-app.locals.basedir = app.get('views');
+// Set Pug as the view engine and define the views directory
+app.set("view engine", "pug");
+app.set("views", "./views");
+app.locals.basedir = app.get("views");
 
-// Register Websocket
+// Register WebSocket with the Express app
 expressWs(app);
 
-// Connect to MongoDB
+// Function to connect to MongoDB and return the collection
 async function connectToMongoDB() {
-    await client.connect();
-    console.log("Connected to MongoDB");
-    return client.db("express").collection("reactiveVars");
+  await client.connect();
+  console.log("Connected to MongoDB");
+  return client.db("express").collection("reactiveVars");
 }
 
-// Websocket route
+// WebSocket route for handling real-time reactivity
 app.ws("/reactivity", async (ws, req) => {
-    console.log("Web Socket opened");
-    const col = await connectToMongoDB();
+  console.log("WebSocket connection opened");
+  const col = await connectToMongoDB();
 
-    ws.on("message", async (msg) => {
-        const update = JSON.parse(msg);
-        console.log("Received from browser: ", update);
+  ws.on("message", async (msg) => {
+    const update = JSON.parse(msg);
+    console.log("Received update from browser: ", update);
 
-        // Send immediate response back to the client
-        ws.send(JSON.stringify({ status: "immediateResponse", varName: update.varName, value: update.value }));
+    // Send immediate response back to the client
+    ws.send(
+      JSON.stringify({
+        status: "immediateResponse",
+        varName: update.varName,
+        value: update.value,
+      })
+    );
 
-        // Then update the MongoDB
-        const queryDoc = { "varName": update.varName };
-        const updateDoc = { $set: { "value": update.value } };
-        await col.findOneAndUpdate(queryDoc, updateDoc, { upsert: true });
+    // Update the MongoDB collection
+    const queryDoc = { varName: update.varName };
+    const updateDoc = { $set: { value: update.value } };
+    await col.findOneAndUpdate(queryDoc, updateDoc, { upsert: true });
 
-        // Send MongoDB update complete message
-        ws.send(JSON.stringify({ status: "updateComplete", varName: update.varName, value: update.value }));
-    });
+    // Notify the client that the MongoDB update is complete
+    ws.send(
+      JSON.stringify({
+        status: "updateComplete",
+        varName: update.varName,
+        value: update.value,
+      })
+    );
+  });
 });
 
-// HTTP route
-// HTTP route
+// HTTP route to render the main page with reactive variables
 app.get("/", async (req, res) => {
-    const col = await connectToMongoDB();
-    let cursor = col.find({}).sort({ varName: 1 });
-    const allVariables = await cursor.toArray();
+  const col = await connectToMongoDB();
+  // Retrieve all variables from MongoDB, sorted by varName
+  let cursor = col.find({}).sort({ varName: 1 });
+  const allVariables = await cursor.toArray();
 
-    res.render("reactivityExample", { reactiveVariables: allVariables });
+  // Render the page with the retrieved variables
+  res.render("reactivityExample", { reactiveVariables: allVariables });
 });
 
-
+// Start the server and listen on the specified port
 app.listen(port, () => {
-    console.log(`App listening on port http://localhost:${port}`);
+  console.log(`App listening on port http://localhost:${port}`);
 });
